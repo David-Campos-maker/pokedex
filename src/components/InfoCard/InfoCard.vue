d746395c3892cff8aecdd49c600b2cfe9adfbce8
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-03-23 16:30:22 -0300

    Details Modal

--- src/components/InfoCard/InfoCard.vue
+++ src/components/InfoCard/InfoCard.vue
@@ -32,60 +32,7 @@
             </button>
 
             <!-- Modal -->
-            <div class="modal fade" ref="info-modal" tabindex="-1" aria-labelledby="" aria-hidden="true">
-                <div class="modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5 info-card__modal-name" id="exampleModalLabel">
-                                {{ pokemon?.name }}
-                            </h1>
-                            <button @click="clearMoves()" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body show-details__container">
-                            <div class="show-details__general-info">
-                                <div class="show-details__pokemon-form">
-                                    <div class="show-details__pokemon-sprite">
-                                        <div class="show-details__sprite">
-                                            <img class="show-details__pokemon-sprite__img pokedex__pokemon-sprites__img" 
-                                            :src="pokemon?.sprites.default" alt="sprite">
-                                        </div>
-                                        <div class="show-details__sprite-shiny">
-                                            <img class="show-details__pokemon-sprite__img pokedex__pokemon-sprites__img" 
-                                            :src="pokemon?.sprites.shiny" alt="sprite">
-                                        </div>
-                                    </div>
-                                    <div class="show-details__pokemon-entries">
-                                        {{ entries }}
-                                    </div> 
-                                    <div class="show-details__pokemon-abilities">
-                                        <div v-for="ability of pokemon?.abilities" :key="ability.ability">
-                                            <div class="info-card__pokemon-abilities__hidden" v-if="ability.is_hidden">
-                                                {{ ability.ability }}
-                                            </div>
-                                            <div class="info-card__pokemon-abilities" v-else> 
-                                                {{ ability.ability }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="show-details__stat-card">
-                                        <stats-card v-bind:pokemonStats="pokemon?.pokemonStats"></stats-card>
-                                    </div>
-                                </div>
-                                <div class="show-details__move-container">
-                                    <move-card v-if="moves.length > 0" :moves="moves"></move-card>
-                                </div>
-                            </div>
-                            <div class="show-details__evolution__container">
-                                <evolution-card v-if="evolution" :evolution="evolution"></evolution-card>
-                            </div>
-                        </div>
-
-                        <div class="modal-footer">
-                            <types-card v-if="pokemon?.pokemonTypes" :types="pokemon.pokemonTypes"></types-card>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <DetailsModal :pokemon="pokemon" ref="modal"></DetailsModal>
         </div>
     </div>
   
@@ -94,22 +41,13 @@
 <script lang="ts">
     import { defineComponent } from "vue";
     import Pokemon from "../../entities/Pokemon";
-    import type IMoves from '../../Interfaces/IMoves';
-    import api from "../../services/api";
-    import MoveCard from "../MovesCard/MoveCard.vue";
-    import StatsCard from "../StatsCard/StatsCard.vue";
     import TypesCard from "../TypesCard/TypesCard.vue";
-    import EvolutionCard from "../EvolutionCard/EvolutionCard.vue";
-    import getEvolutionTree from "../../functions/getEvolutionTree";
-    import getEvolutionChainUrl from "../../functions/getEvolutionChainUrl";
-    import type IEvolution from "../../Interfaces/IEvolution";
+    import DetailsModal from "../Modal/DetailsModal.vue";
 
     export default  defineComponent({
-        components: { 
-            MoveCard , 
-            StatsCard , 
-            EvolutionCard , 
-            TypesCard
+        components: {  
+            TypesCard , 
+            DetailsModal 
         } ,
 
         name: "info-card" ,
@@ -118,73 +56,10 @@
             pokemon: Pokemon,
         },
 
-        data() {
-            let modalRef: any = null;
-            let moves: Array<IMoves> = [];
-            let evolution = {} as IEvolution;
-            let entries = '';
-
-            return {
-                modalRef ,
-                moves ,
-                evolution ,
-                entries
-            }
-        },
-
-        mounted() {
-            this.$refs["info-modal"];
-        },
-
         methods: {
-            toggleModal() {
-                if(!this.modalRef) {
-                    this.modalRef = new (window as any).bootstrap.Modal(this.$refs["info-modal"]);
-                }
-                this.modalRef.toggle();
-            },
-
-            async getEntriesText() {
-                const response = await api.get(this.pokemon!.speciesUrl);
-
-                for(const element of response.data.flavor_text_entries) {
-                    if (element.language.name == 'en') {
-                        this.entries = element.flavor_text;
-                        break;
-                    }
-                }
-            },
-
-            async getMoves() {
-                const moves = await Promise.all(this.pokemon!.moves.map(async (move: string) => {
-                    const res = await api.get(move);
-
-                    return {
-                        name: res.data.name.replace(/-/g , " "),
-                        accuracy: res.data.accuracy,
-                        damage_class: res.data.damage_class.name,
-                        power: res.data.power,
-                        pp: res.data.pp,
-                        type: res.data.type.name
-                    };
-                }));
-                this.moves = moves;
-            },
-
-            async getEvolutions() {
-                const res = await api.get(await getEvolutionChainUrl(this.pokemon!.speciesUrl));
-                this.evolution = await getEvolutionTree(res.data.chain);
-            },
-
-            clearMoves() {
-                this.moves = [];
-            },
-
             async modalTrigger() {
-                await this.getEntriesText();
-                await this.getEvolutions();
-                await this.getMoves();
-                this.toggleModal();
+                (this.$refs.modal as InstanceType<typeof DetailsModal>).fetchData();
+                (this.$refs.modal as InstanceType<typeof DetailsModal>).showModal();
             }
         }
     })
---
+++ src/components/Modal/DetailsModal.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="modal fade" ref="info-modal" tabindex="-1" aria-labelledby="" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5 show-details__pokemon-name" id="exampleModalLabel">
+                {{ pokemon?.name }}
+            </h1>
+            <button @click="clearMoves()" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body show-details__container">
+            <div class="show-details__general-info">
+                <div class="show-details__pokemon-form">
+                    <div class="show-details__pokemon-sprite">
+                        <div class="show-details__sprite">
+                            <img class="show-details__pokemon-sprite__img pokedex__pokemon-sprites__img" 
+                            :src="pokemon?.sprites.default" alt="sprite">
+                        </div>
+                        <div class="show-details__sprite-shiny">
+                            <img class="show-details__pokemon-sprite__img pokedex__pokemon-sprites__img" 
+                            :src="pokemon?.sprites.shiny" alt="sprite">
+                        </div>
+                    </div>
+                    <div class="show-details__pokemon-entries">
+                        {{ entries }}
+                    </div> 
+                    <div class="show-details__pokemon-abilities">
+                        <div v-for="ability of pokemon?.abilities" :key="ability.ability">
+                            <div class="show-details__pokemon-abilities__ability-hidden" v-if="ability.is_hidden">
+                                {{ ability.ability }}
+                            </div>
+                            <div class="show-details__pokemon-abilities__ability" v-else> 
+                                {{ ability.ability }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="show-details__stat-card">
+                        <stats-card v-bind:pokemonStats="pokemon?.pokemonStats"></stats-card>
+                    </div>
+                </div>
+                <div class="show-details__move-container">
+                    <move-card v-if="moves.length > 0" :moves="moves"></move-card>
+                </div>
+            </div>
+            <div class="show-details__evolution__container">
+                <evolution-card v-if="evolution" :evolution="evolution"></evolution-card>
+            </div>
+        </div>
+
+        <div class="modal-footer">
+            <types-card v-if="pokemon?.pokemonTypes" :types="pokemon.pokemonTypes"></types-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Pokemon from '@/entities/Pokemon';
+  import { defineComponent } from 'vue';
+  import MoveCard from "../MovesCard/MoveCard.vue";
+  import StatsCard from "../StatsCard/StatsCard.vue";
+  import EvolutionCard from "../EvolutionCard/EvolutionCard.vue";
+  import TypesCard from "../TypesCard/TypesCard.vue";
+  import type IMoves from '../../Interfaces/IMoves';
+  import api from "../../services/api";
+  import getEvolutionTree from "../../functions/getEvolutionTree";
+  import getEvolutionChainUrl from "../../functions/getEvolutionChainUrl";
+  import type IEvolution from "../../Interfaces/IEvolution";
+
+  export default defineComponent({
+    components: {
+      MoveCard , 
+      StatsCard , 
+      EvolutionCard , 
+      TypesCard , 
+    },
+
+    name:"details-modal",
+
+    data() {
+      let modalRef: any = null;
+      let moves: Array<IMoves> = [];
+      let evolution = {} as IEvolution;
+      let entries = '';
+
+      return {
+        modalRef ,
+        moves ,
+        evolution ,
+        entries
+      }
+    },
+
+    props: {
+      pokemon: Pokemon
+    },
+
+    methods: {
+      async getEntriesText() {
+        const response = await api.get(this.pokemon!.speciesUrl);
+
+        for(const element of response.data.flavor_text_entries) {
+            if (element.language.name == 'en') {
+                this.entries = element.flavor_text;
+                break;
+            }
+        }
+    },
+
+      async getMoves() {
+        const moves = await Promise.all(this.pokemon!.moves.map(async (move: string) => {
+          const res = await api.get(move);
+
+          return {
+            name: res.data.name.replace(/-/g , " "),
+            accuracy: res.data.accuracy,
+            damage_class: res.data.damage_class.name,
+            power: res.data.power,
+            pp: res.data.pp,
+            type: res.data.type.name
+          };
+        }));
+        this.moves = moves;
+      },
+
+      async getEvolutions() {
+        const res = await api.get(await getEvolutionChainUrl(this.pokemon!.speciesUrl));
+        this.evolution = await getEvolutionTree(res.data.chain);
+      },
+
+      clearMoves() {
+        this.moves = [];
+      },
+
+      async fetchData() {
+        await this.getEntriesText();
+        await this.getEvolutions();
+        await this.getMoves();
+      },
+
+      async showModal() {
+        if(!this.modalRef) {
+          this.modalRef = new (window as any).bootstrap.Modal(this.$refs["info-modal"]);
+        }
+        this.modalRef.toggle();
+      }
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+    @import "./styles.scss";
+</style>
